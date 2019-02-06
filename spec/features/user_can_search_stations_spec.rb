b12408e3501f0ce77c8efead829d7f05783f7878
Leah Miller <lkmiller.87@gmail.com>
2019-02-06 09:30:52 -0700

    Adds to feature test setup

--- spec/features/user_can_search_stations_spec.rb
+++ spec/features/user_can_search_stations_spec.rb
@@ -9,11 +9,21 @@ feature 'user can visit root page' do
    fill_in :q, with: 80203
   # And I click "Locate"
    click_on "Locate"
-  # Then I should be on page "/search"
-  expect(current_path).to eq("/search")
-  # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-
-  # And the stations should be limited to Electric and Propane
-  # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    # Then I should be on page "/search"
+    expect(current_path).to eq("/search")
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    expect(page).to have_content("10 Closest Stations")
+    expect(page).to have_css(".station", count: 10)
+    # And the stations should be limited to Electric and Propane
+    expect(page).to have_content("ELEC")
+    expect(page).to have_content("LPG")
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    within(first(".station"))do
+      expect(page).to have_css(".name")
+      expect(page).to have_css(".address")
+      expect(page).to have_css(".fuel_types")
+      expect(page).to have_css(".distance")
+      expect(page).to have_css(".access_times")
+    end
   end
 end
