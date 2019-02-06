86105dbdfb2c0aa86cf59567e4d5beabd8877498
Leah Miller <lkmiller.87@gmail.com>
2019-02-06 09:23:00 -0700

    Adds passing tests for initial test setup

--- spec/features/user_can_search_stations_spec.rb
+++ spec/features/user_can_search_stations_spec.rb
@@ -12,6 +12,7 @@ feature 'user can visit root page' do
   # Then I should be on page "/search"
   expect(current_path).to eq("/search")
   # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+
   # And the stations should be limited to Electric and Propane
   # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
   end
